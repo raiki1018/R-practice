a84a2f175384bd8474d498b46e507c44de3f70d1
raiki1018 <traiki91018@gmail.com>
2021-11-24 15:39:46 +0900

    test

---
+++ js/jspractice.js
@@ -0,0 +1,17 @@
+// document.write("Hello World");
+// alert("Hello!");
+// console.log("Hello World" + "田中さん");
+
+// console.log(1 + 1);
+// console.log(1 - 1);
+// console.log(2 * 2);
+
+// let x = 1;
+// let y = 1;
+// x++;
+// console.log(x);
+// y--;
+// console.log(y);
+
+let age = 30;
+const name = "田中太郎";
